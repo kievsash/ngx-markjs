2457e48c6baade99239c038adce15ba76e6bdc2a
Oleksandr Poshtaruk <kievsash@gmail.com>
2019-10-03 17:30:21 +0300

    demo page

--- projects/ngx-markjs/src/lib/markjs-highlight.directive.ts
+++ projects/ngx-markjs/src/lib/markjs-highlight.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import {Directive} from '@angular/core';
+
+declare var require: any;
+const Mark = require('mark.js');
 
 @Directive({
   selector: '[libMarkjsHighlight]'
 })
 export class MarkjsHighlightDirective {
 
-  constructor() { }
+  constructor() {
+  }
 
 }
+
